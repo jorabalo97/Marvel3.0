6215a7bcd515f74f77a902b69a68a4d3bf7529a4
jorabalo97 <jorabalo97@gmail.com>
2023-12-15 11:16:28 +0100

    Initial Commit

---
+++ Marvel3.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel3.0
+//
+//  Created by Jorge Abalo Dieste on 15/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Marvel3.0UITests/Marvel3_0UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Marvel3_0UITestsLaunchTests.swift
+//  Marvel3.0UITests
+//
+//  Created by Jorge Abalo Dieste on 15/12/23.
+//
+
+import XCTest
+
+final class Marvel3_0UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
